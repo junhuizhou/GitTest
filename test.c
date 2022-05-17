e6832552544a11cea729f07ba9c2e2e70d660058
junhuizhou <1837981773@qq.com>
2022-05-17 11:35:28 +0800

    测试md区块嵌套

--- test.c
+++ test.c
@@ -1,6 +1,15 @@
+/*
+ * @Author: junhuizhou
+ * @Date: 2022-05-09 17:20:24
+ * @LastEditor: junhuizhou
+ * @LastEditTime: 2022-05-17 11:32:07
+ * @Description: file content
+ * @FilePath: \GitTest\test.c
+ */
 
-//BUG 文件头部注释快捷键失效，尝试换掉多个快捷键无果
 int main()
 {
     return 0;
 }
+
+//TAG: 文件头部注释快捷键莫名其妙又能用了，我啥也没动
