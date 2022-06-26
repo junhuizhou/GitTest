7441a5db91e1b64d2d265b4ecf63e27d8ef1c343
ZhouJunhui <1837981773@qq.com>
2022-06-26 17:27:51 +0800

    add admin@DESKTOP-E8PFF9E

--- test.c
+++ test.c
@@ -1,15 +1,23 @@
 /*
- * @Author: junhuizhou
- * @Date: 2022-05-09 17:20:24
- * @LastEditor: junhuizhou
- * @LastEditTime: 2022-05-17 11:32:07
- * @Description: file content
+ * @Author: ZhouJunhui
+ * @Date: 2022-06-26 16:37:09
+ * @LastEditor: ZhouJunhui
+ * @LastEditTime: 2022-06-26 17:18:08
  * @FilePath: \GitTest\test.c
+ * @Description: file content
  */
 
-int main()
+#include <stdio.h>
+
+/**
+ * @description: 
+ * @algorithm: 
+ * @param {int} argc
+ * @param {char} *argv
+ * @return {*}
+ */
+int main(int argc, char *argv[])
 {
+    printf("hello world\n");
     return 0;
 }
-
-//TAG: 文件头部注释快捷键莫名其妙又能用了，我啥也没动
